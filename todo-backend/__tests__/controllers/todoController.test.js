09cc280e47a6ac210111f5864a2f1b6087ffed11
Sanyam134 <xyz@gmail.com>
2024-12-14 18:51:36 +0000

    done

--- todo-backend/__tests__/controllers/todoController.test.js
+++ todo-backend/__tests__/controllers/todoController.test.js
@@ -1,8 +1,11 @@
-const todoController = require ("../Controllers/todoController")
+const todoController = require ("../../Controllers/todoController")
 
 describe("When todo is invoked", () =>{
-      describe("For getTodos function", () =>{
-        expect(true).tobe(true)
-      })
+    describe("For getTodos function", () =>{
+       it("Should return me all todos", ()=>{
+        expect(true).toBe(true)
+       }) 
+        
+    })
 
 })
